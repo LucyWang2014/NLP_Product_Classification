b5eeb951ab8141243e10e269584ec40bf553479b
Charles Guthrie <charles.d.guthrie@gmail.com>
2015-11-20 11:41:20 -0500

    added download_images_to_file.py

---
+++ scripts/download_images_to_file.py
@@ -0,0 +1,75 @@
+'''
+download_images.py
+'''
+__author__="Charlie Guthrie"
+
+import os,sys
+import cPickle as pkl
+from datetime import datetime
+
+import pandas as pd
+import numpy as np
+
+#image downloading and processing
+from PIL import Image
+import urllib, cStringIO
+
+def download_and_resize_image(url,width):
+    '''
+    download and resize, convert to numpy array
+    
+    args:
+        url: url of image
+        width: desired width of image
+    returns:
+        np.array formatted version of image
+    '''
+    print "getting image from url: %s ..." %url
+    try:
+        img_file = cStringIO.StringIO(urllib.urlopen(url).read())
+        img = Image.open(img_file)
+        resized = img.thumbnail((width,width), Image.ANTIALIAS)
+        return np.array(img)
+    except:
+        print "image not downloaded"
+        print sys.exc_info()[0]
+        return None
+
+def add_all_images(datadir,infile_name,outfile_name,width=64):
+    '''
+    download and resize images and insert them into the data frame
+    also saves new data frame to disc
+
+    args:
+        datadir: data directory
+        infile_name: csv input file name
+        outfile_name: csv output file name
+        width: desired width of image
+    returns:
+        data frame with image
+    '''
+    csv_path = datadir + infile_name
+    textDF = pd.read_csv(csv_path,header = 0, index_col = 0,low_memory = False)
+    imageDF = pd.DataFrame(textDF.large_image_URL.apply(download_and_resize_image,args=(width,)))
+    with open(datadir + outfile_name, 'wb') as outf:
+        pkl.dump(imageDF,outf)
+    imageDF.to_csv(datadir + outfile_name+'.csv')
+    return textDF,imageDF
+
+def main():
+    start_time = datetime.now()
+
+    HOME = os.path.join(os.getcwd(),'..')
+    DATADIR = os.path.join(HOME,'data') + '/'
+    INFILE = 'head_train_set.csv'
+    OUTFILE = 'head_train_images.pkl'
+
+    #get all the images
+    add_all_images(DATADIR,INFILE,OUTFILE)
+    
+    end_time = datetime.now()
+    runtime = end_time - start_time
+    print "Script took",runtime
+
+if __name__ == '__main__':
+    main()

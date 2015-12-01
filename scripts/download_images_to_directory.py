d6c8a497be9c2b3443710d5b31b213db9b5502c0
Charles Guthrie <charles.d.guthrie@gmail.com>
2015-11-30 21:52:13 -0500

    fixed download_images

--- scripts/download_images_to_directory.py
+++ scripts/download_images_to_directory.py
@@ -3,56 +3,51 @@ download_images_to_directory.py
 '''
 __author__="Charlie Guthrie"
 
-import os
-import sys
+import os,sys
 from datetime import datetime
-
 import pandas as pd
 import numpy as np
+import cPickle as pkl
 
 #image downloading and processing
-from PIL import Image
-import urllib, cStringIO
-
+import urllib
 import io
+import matplotlib.pyplot as plt
 import skimage.transform
 
-def download_and_resize(url):
+def download_and_resize(url,width):
     '''
+    download image url, resize and crop to [width]
+    args:
+        url: url of image
+        width: width of square image
+    returns:
+        rawim: image cropped and resized to width square
     '''
     ext = url.split('.')[-1]
     im = plt.imread(io.BytesIO(urllib.urlopen(url).read()), ext)
     # Resize so smallest dim = 256, preserving aspect ratio
     h, w, _ = im.shape
     if h < w:
-        im = skimage.transform.resize(im, (256, w*256/h), preserve_range=True)
+        im = skimage.transform.resize(im, (width, w*width/h), preserve_range=True)
     else:
-        im = skimage.transform.resize(im, (h*256/w, 256), preserve_range=True)
+        im = skimage.transform.resize(im, (h*width/w, width), preserve_range=True)
 
-    # Central crop to 224x224
+    # Central crop to width x width
+    halfwidth = width/2
     h, w, _ = im.shape
-    im = im[h//2-112:h//2+112, w//2-112:w//2+112]
-    
+    im = im[h//2-halfwidth:h//2+halfwidth, w//2-halfwidth:w//2+halfwidth]
+
     #rawim = np.copy(im).astype('uint8')
-    rawim = np.astype('uint8')
+    rawim = im.astype('uint8')
     return rawim
- 
-def process_for_rnn(im):
-    # Shuffle axes to c01
-    im = np.swapaxes(np.swapaxes(im, 1, 2), 0, 1)
-    
-    # Convert to BGR
-    im = im[::-1, :, :]
 
-    im = im - MEAN_IMAGE
-    return floatX(im[np.newaxis])
-
-def prep_image(url,idx,dataset,datadir,width,filetype='bmp'):
+def prep_image(url,idx,dataset,datadir,width=224,filetype='jpg'):
     '''
     Check to see image file has been downloaded at current size.  If it has not,
     download and resize image. Saves file to datadir/images/[dataset]_[idx]_w[width].[filetype]
     e.g. datadir/images/train_10001_w256.bmp
-    
+
     args:
         url: url of image source
         idx: image row index
@@ -61,23 +56,26 @@ def prep_image(url,idx,dataset,datadir,width,filetype='bmp'):
         width: desired width of image. Will be resized to width squared
     returns:
         rawim: scaled and cropped image
-        im: processed image to input into VGG
     '''
     outpath = datadir + 'images/' + dataset + '_' +  str(idx) + '_w' + str(width) + '.' + filetype
-    
+
     if not os.path.isfile(outpath):
         print "downloading image #%s..." %str(idx)
         try:
-            rawim,im = prep_image(url)
+            rawim = download_and_resize(url,width)
+            plt.imsave(outpath,rawim)
+            return rawim
         except:
             print "unable to download"
     else:
-        print "loading image %i from file..." % idx
+        print "Image %i already downloaded. Loading from file..." % idx
+        rawim = plt.imread(outpath)
+        return rawim
         
-
-def get_selected_images(csv_path,first_idx,last_idx,dataset,datadir,width=64,filetype='bmp'):
+def get_selected_images(csv_path,first_idx,last_idx,dataset,datadir,width=224,filetype='jpg'):
     '''
-    for a given index range, download and resize the images
+    for a given index range, download and resize the images,
+    then save to directory
 
     args:
         csv_path: path to csv
@@ -91,29 +89,39 @@ def get_selected_images(csv_path,first_idx,last_idx,dataset,datadir,width=64,fil
     data = pd.read_csv(csv_path,header = 0, index_col = 0,low_memory = False)
     image_urls = data.large_image_URL.loc[first_idx:last_idx]
     for i,url in image_urls.iteritems():
-        download_and_resize(url,i,dataset,datadir,width,filetype)
+        prep_image(url,i,dataset,datadir,width,filetype)
 
-def main(first_idx,last_idx):
+def main(csv_name,dataset,first_idx,last_idx):
     start_time = datetime.now()
 
     home = os.path.join(os.path.dirname(__file__),'..')
-    #datadir = os.path.join(home,'data') + '/'
-    
+    #local datadir
+    datadir = os.path.join(home,'data') + '/'
+
     #hpc datadir
-    datadir = '/scratch/cdg356/spring/data/'
-    csv_path = datadir + 'train_set.csv'
+    #datadir = '/scratch/cdg356/spring/data/'
     
-    get_selected_images(csv_path,first_idx,last_idx,'train',datadir)
-    
+    csv_path = datadir + csv_name
+
+    get_selected_images(csv_path,first_idx,last_idx,dataset,datadir)
+
     end_time = datetime.now()
     runtime = end_time - start_time
     print "Script took",runtime
 
 if __name__ == '__main__':
 
-    if len(sys.argv)<3:
-        main(None,None)
+    if len(sys.argv)<2:
+        print "usage: python csv_file dataset [first_idx] [last_idx]"
+        print "example: python train_set.csv train"
+        main('train_set.csv','train',None,None)
+    elif len(sys.argv)<4:
+        csv_name = sys.argv[1]
+        dataset = sys.argv[2]
+        main(csv_name,dataset,None,None)
     else:
-        first_idx = int(sys.argv[1])
-        last_idx = int(sys.argv[2])
-        main(first_idx,last_idx)
+        csv_name = sys.argv[1]
+        dataset = sys.argv[2]
+        first_idx = int(sys.argv[3])
+        last_idx = int(sys.argv[4])
+        main(csv_name,dataset,first_idx,last_idx)

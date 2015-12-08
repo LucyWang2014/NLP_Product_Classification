960b5a649f2aaed9ec2432a1191b45cef7af6e61
Charles Guthrie <charles.d.guthrie@gmail.com>
2015-12-07 22:50:00 -0500

    fixed utils

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,22 +1,22 @@
 #LOGGING BOILERPLATE
 from datetime import datetime
-start_time = datetime.now()
 import os
-
-#logging boilerplate.  To log anything type log.info('stuff you want to log')
 import logging as log
 
+#logging boilerplate.  To log anything type log.info('stuff you want to log')
 
-def create_log(file_name):
-	#make log file if it doesn't exist
-	if not os.path.exists('../logs'):
-		os.makedirs('../logs')
+def create_log(script_name):
+    start_time = datetime.now()
+    #make log file if it doesn't exist
+    if not os.path.exists('../logs'):
+        os.makedirs('../logs')
 
-	#log file with filename, HMS time
-	log.basicConfig(filename='logs/%s%s.log' %(file_name.split('.')[0],start_time.strftime('_%Y%m%d_%H%M%S')),level=log.DEBUG)
+    #log file with filename, HMS time
+    log_time = start_time.strftime('_%Y%m%d_%H%M%S')
+    log_fname = '../logs/%s%s.log' %(script_name.split('.')[0],log_time)
+    log.basicConfig(filename=log_fname,level=log.DEBUG)
 
-	return log
 
 def plog(msg):
-	print msg
-	log.info(msg)
+    print msg
+    log.info(msg)
